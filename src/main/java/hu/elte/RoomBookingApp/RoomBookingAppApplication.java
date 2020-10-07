a7667bc6a8912d32e6e258dc3ee9d314a48d38ee
Szabó Antal Balázs <idkbalazs@gmail.com>
2020-10-07 19:38:07 +0200

    Init springboot project

---
+++ src/main/java/hu/elte/RoomBookingApp/RoomBookingAppApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.RoomBookingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomBookingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/elte/RoomBookingApp/RoomBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.elte.RoomBookingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
