f156b86a9e590627b2b9f8e77b8913d5d03b4615
Szabó Antal Balázs <idkbalazs@gmail.com>
2020-10-08 22:38:20 +0200

    Added classes for the room part of the backend

--- src/main/java/hu/elte/RoomBookingApp/Entities/Room.java
+++ src/main/java/hu/elte/RoomBookingApp/Entities/Room.java
@@ -1,1 +1,37 @@
-//TODOS
+package hu.elte.RoomBookingApp.Entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer iD;
+
+    @Column
+    @NotNull
+    private Integer capacity;
+
+    @Column
+    @NotNull
+    private Integer floor;
+
+    @Column
+    @NotNull
+    private boolean reserved;
+
+}
