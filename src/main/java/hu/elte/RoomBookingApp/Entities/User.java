b6c97ffe21f79153ab77520f37dbb97b0cc9d315
MiklosDavid <miklosdavid12@gmail.com>
2020-10-27 19:08:45 +0100

    User-Booking-Room connectons corrected

--- src/main/java/hu/elte/RoomBookingApp/Entities/User.java
+++ src/main/java/hu/elte/RoomBookingApp/Entities/User.java
@@ -7,7 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-
+import java.util.List;
 import javax.validation.constraints.NotNull;
 
 @Entity
